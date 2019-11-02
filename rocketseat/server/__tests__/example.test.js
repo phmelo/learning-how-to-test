ad9a05a6bb3293f094ec5c8da713b294b9932d1a
Paulo Henrique Melo de Morais <phmelomorais@gmail.com>
2019-11-02 19:17:33 -0300

    first commit

---
+++ rocketseat/server/__tests__/example.test.js
@@ -0,0 +1,8 @@
+function soma(a, b) {
+  return a + b;
+}
+
+test('If I call soma function with 4 and 5, should return 9', () => {
+  const result = soma(4, 5);
+  expect(result).toBe(9);
+});
---
+++ rocketseat/server/bootstrap.js
@@ -0,0 +1,6 @@
+// import dotenv from 'dotenv';
+const dotenv = require('dotenv');
+
+dotenv.config({
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
+});
