3c038f08bda15a44bd33f9b4a6b0904ce4e2a8e4
Paulo <phmelomorais@gmail.com>
2019-11-03 03:12:08 -0300

    TDD - First Unit test on TechList component

--- rocketseat/web-react-app/src/__tests__/App.test.js
+++
@@ -1,6 +0,0 @@
-//import App from "~/App";
-//const expectExport = require("expect");
-
-test("example", () => {
-  expect(1 + 1).toBe(2);
-});
---
+++ rocketseat/web-react-app/src/components/TechList/index.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+export default function TechList() {
+  const [techs, setTechs] = useState([]);
+
+  function handleAddTech() {
+    setTechs([...techs, "Node.JS"]);
+  }
+
+  return (
+    <div>
+      <ul data-testid="tech-list">
+        {techs.map(tech => (
+          <li key={tech}>{tech}</li>
+        ))}
+      </ul>
+      <button type="button" onClick={handleAddTech}>
+        Adicionar
+      </button>
+    </div>
+  );
+}
