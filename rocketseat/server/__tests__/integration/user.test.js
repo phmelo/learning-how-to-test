4801fb66300c2a9f1b82540197791034515d48fc
Paulo <phmelomorais@gmail.com>
2019-11-02 23:29:38 -0300

    TDD - It should not be able to register a user with a existing email

--- rocketseat/server/__tests__/integration/user.test.js
+++ rocketseat/server/__tests__/integration/user.test.js
@@ -13,4 +13,24 @@ describe('User', () => {
 
     expect(response.body).toHaveProperty('id');
   });
+
+  it('should not be able to register with a existing email', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: 'Paulo Henrique',
+        email: 'ph@phdev.com.br',
+        password_hash: '123456',
+      });
+
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Paulo Henrique',
+        email: 'ph@phdev.com.br',
+        password_hash: '123456',
+      });
+
+    expect(response.status).toBe(400);
+  });
 });
