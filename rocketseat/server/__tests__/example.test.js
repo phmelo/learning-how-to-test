134b2ec4c16d7ab5f984ffc01f4d6e64dc726a20
Paulo <phmelomorais@gmail.com>
2019-11-02 22:24:34 -0300

    Configuring environment variables and setting up sqlite

--- rocketseat/server/__tests__/example.test.js
+++ rocketseat/server/__tests__/example.test.js
@@ -1,8 +1,18 @@
 function soma(a, b) {
+  console.log(process.env.NODE_ENV);
   return a + b;
+}
+
+function getNodeEnv() {
+  return process.env.NODE_ENV || 'postgres';
 }
 
 test('If I call soma function with 4 and 5, should return 9', () => {
   const result = soma(4, 5);
   expect(result).toBe(9);
+});
+
+test('If I call getNodeEnv method, should return test', () => {
+  const result = getNodeEnv();
+  expect(result).toBe('test');
 });
--- rocketseat/server/src/app.js
+++ rocketseat/server/src/app.js
@@ -1,4 +1,3 @@
-// import 'dotenv/config';
 import './bootstrap';
 import Youch from 'youch';
 import express from 'express';
