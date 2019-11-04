0b401e0a7f90080e7af7ecd2fcdb6115553ed246
Paulo <phmelomorais@gmail.com>
2019-11-03 21:23:40 -0300

    TDD - Testing TechList on React Native component - Test Succeed

--- rocketseat/mobileRN/src/components/TechList.js
+++ rocketseat/mobileRN/src/components/TechList.js
@@ -1,8 +1,31 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useState} from 'react';
+import {View, TextInput, TouchableOpacity, Text, FlatList} from 'react-native';
 
 // import { Container } from './styles';
 
 export default function TechList() {
-  return <View />;
+  const [techs, setTechs] = useState([]);
+  const [newTech, setNewTech] = useState('');
+
+  function handleAdd() {
+    setTechs([...techs, newTech]);
+  }
+
+  return (
+    <View>
+      <FlatList
+        data={techs}
+        keyExtractor={tech => tech}
+        renderItem={({item}) => <Text>{item}</Text>}
+      />
+      <TextInput
+        testID="tech-input"
+        value={newTech}
+        onChangeText={setNewTech}
+      />
+      <TouchableOpacity onPress={handleAdd}>
+        <Text>Adicionar</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
